05f374c975df3a9c75a279208e730ce1fe2cba96
Theeranat Sreecharoen <theeranatsre@gmail.com>
2022-08-11 23:21:33 +0700

    feature : add button menu

---
+++ lib/screens/CreateMenuScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CreateMenuScreen extends StatefulWidget {
+  const CreateMenuScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CreateMenuScreen> createState() => _CreateMenuScreenState();
+}
+
+class _CreateMenuScreenState extends State<CreateMenuScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/screens/SearchMenuScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SearchMenuScreen extends StatefulWidget {
+  const SearchMenuScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SearchMenuScreen> createState() => _SearchMenuScreenState();
+}
+
+class _SearchMenuScreenState extends State<SearchMenuScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
